153c7cca1517e9c42ec8615a0c5bb0d2e44a3bca
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2023-07-11 11:30:59 +0300

    chore: add .gitignore file chore: add .npmrc file docs: add README.md file feat: add app.vue file feat: add main.css file feat: add default.vue file feat: add nuxt.config.ts file
    chore(package.json): add package.json file with necessary scripts and devDependencies
    chore(postcss.config.js): add postcss.config.js file with tailwindcss and autoprefixer plugins
    chore(public): add favicon.ico file
    chore(server/tsconfig.json): add server/tsconfig.json file with extends property
    chore(tailwind.config.js): add tailwind.config.js file with content and theme properties
    chore(tsconfig.json): add tsconfig.json file with extends property

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ['~/assets/css/main.css'],
+  postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  modules: [],
+  devtools: { enabled: true }
+})
